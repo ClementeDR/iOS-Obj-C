2452b5dca5216033f859ca957f1fb682a44b65db
Clemente Di Rosa <dirosaclemente96@gmail.com>
2017-01-23 19:45:44 +0100

    Esercisio5

--- Esercizi-23-01-17/Esercizio5/Esercizio5/ViewController.m
+++ Esercizi-23-01-17/Esercizio5/Esercizio5/ViewController.m
@@ -8,8 +8,21 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+/*
+ 5) dato un array di nomi
 
+ 
+ 
+ 
+ inserirli in un array, farne il sort in ordine alfabetico e loggarne il risultato.
+ PLUS: dividere in 2 array distinti i nomi dai cognomi
+*/
+
+@interface ViewController (){
+    NSArray *_myArray;
+    NSMutableArray *_myNameArray;
+    NSMutableArray *_mySurnameArray;
+}
 @end
 
 @implementation ViewController
@@ -17,13 +30,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
+    
+    _myArray = @[@"Freddie Campbell", @"Krista Kim", @"Merle Hubbard", @"Lorraine Weber", @"Lorenzo Greene", @"Sandra Brock",@"Nichole Hines",@"Floyd Clarke",@"Andrea Ramsey", @"Marcus Reid", @"Jacob Garza", @"Peter Herrera", @"Glenn Horton", @"Francis Mack",@"Yvette Farmer",@"Evan Hughes",@"Shelley Lane",@"Gladys Carroll",@"Stella Thornton", @"Mindy Wilkerson",@"Beth Diaz", @"Everett Mckinney",@"Audrey Cooper",@"Christie Sutton",@"Timothy Walters"];
+    
+    _myArray = [_myArray sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+    _myNameArray = [[NSMutableArray alloc] init];
+    _mySurnameArray = [[NSMutableArray alloc] init];
+    NSLog(@"array ordered %@",_myArray);
+    
+    for (int i = 0; i < _myArray.count; i++) {
+        NSArray  *data = [_myArray[i] componentsSeparatedByString:@" "];
+        [_myNameArray addObject:data[0]];
+        [_mySurnameArray addObject:data[1]];
+    }
+    
+    for (int j = 0; j < _myArray.count; j++) {
+        NSLog(@"Name : %@ - Surname : %@", _myNameArray[j], _mySurnameArray[j]);
+    }
 
+    
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(NSArray *)orderArray:(NSArray *)array{
+    NSArray *newArray = [array sortedArrayUsingComparator:^NSComparisonResult(NSString  *obj1, NSString  *obj2) {
+    
+        if (obj1.lowercaseString == obj2.lowercaseString) {
+            return NSOrderedSame;
+        } else {
+            if (obj1.lowercaseString < obj2.lowercaseString) {
+                return NSOrderedAscending;
+            } else {
+                return NSOrderedDescending;
+            }
+        }
+    }];
+    NSLog(@"aaaa %@", newArray);
+    return newArray;
+}
+
+
 
 
 @end
